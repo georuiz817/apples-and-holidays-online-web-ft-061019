a5d7a5dcc3ac6ee242bf150114bd50806f728a4a
George Ruiz <georuiz817@gmail.com>
2019-06-16 22:59:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -14,8 +14,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:winter][:Christmas, :new_years] = supply_array
-  holiday_hash
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
